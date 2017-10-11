ea16a9b42a8af0788b974bb38ab3dd42194baeaa
James Soo <jamesklsoo@gmail.com>
2017-10-11 10:49:48 +0800

    update portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-function swapImage(){
-    var swapImage = $('#switch').attr('data-swap'),
-        currentImage = $('#switch').attr('src');
-
-    $('#switch').attr({
-        'src': swapImage,
-        'data-swap': currentImage
-    });
-};
